087d2d6e5c97cd6eaa522e9bc787a70d9369c9ef
Daniel Ruatta <druatta@gmail.com>
2021-05-16 01:12:42 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ExampleConsoleApplication
+{
+    class Program
+    {
+        /// <summary>
+        /// This is Main()
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's play a text based game!\n");
+
+            Console.WriteLine("Player 1: Daniel Ruatta\n");
+
+            Console.WriteLine("Player 2: _____________\n");
+
+            Console.WriteLine("Enter your character's name:\t");
+        }
+
+        // I think I need to get a faster computer, this 8GB of RAM is having trouble
+        // running Chrome and Visual Studio.
+    }
+}
